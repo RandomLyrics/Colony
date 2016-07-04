031670230931495d902bea6bb3ed5c477c0ee8aa
msobieraj <mateusz.sobieraj@eservice.com.pl>
2016-07-04 16:52:12 +0200

    dsfdsfsd

---
+++ Colony/Orion/Example/Logic.cs
@@ -0,0 +1,14 @@
+﻿using Orion.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orion.Example
+{
+    public class Logic: IShareable
+    {
+
+    }
+}
