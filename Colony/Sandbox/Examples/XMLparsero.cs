a55977a0a2c27c54ef7397741fdaa21678f36c9e
RandomLyrics <mateusz.sobieraj@eservice.com.pl>
2016-08-19 17:57:00 +0200

    asdsadsad

---
+++ Colony/Sandbox/Examples/XMLparsero.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Sandbox.Examples
+{
+    public Dictionary< MyProperty { get; set; }
+    public class RolePage
+    {
+        public string Title { get; set; }
+        public List<string> Roles { get; set; }
+        public RolePage Child { get; set; }
+    }
+    public class XMLparsero
+    {
+        string xmlpath = @"Web.sitemap";
+        public void Run()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlpath);
+            
+            var d = doc;
+        }
+
+        public RolePage ConvertToRolePage(XmlNode node)
+        {
+            var rp = new RolePage();
+            //if (node.ChildNodes.Count > 0 )
+            //    rp.Child = ConvertToRolePage(
+            return rp;
+        }
+    }
+
+}
--- Colony/Sandbox/Program.cs
+++ Colony/Sandbox/Program.cs
@@ -26,25 +26,27 @@ namespace Sandbox
         static int amount = 1000000;
         static void Main(string[] args)
         {
-            Random r = new Random();
-            List<Wave> wavelist = new List<Wave>(amount);
-            List<SObject> stacklist = new List<SObject>();
-            Action wave = () =>
-            {
-                wavelist.Add(new Wave());
-                wavelist.Last().height = r.Next(1000000);
-            };
-            Action stack = () =>
-            {
-                stacklist.Add(new SObject(new Wave()));
-                stacklist.Last().val.height = r.Next(1000000);
-            };
+            new XMLparsero().Run();
 
-            Stats.PrintOutTicks(wave, amount);
-            Stats.PrintOutTicks(stack, amount);
-            Stats.PrintOutTicks(wave, amount);
-            Stats.PrintOutTicks(stack, amount);
-            var dupa = 50;
+            //Random r = new Random();
+            //List<Wave> wavelist = new List<Wave>(amount);
+            //List<SObject> stacklist = new List<SObject>();
+            //Action wave = () =>
+            //{
+            //    wavelist.Add(new Wave());
+            //    wavelist.Last().height = r.Next(1000000);
+            //};
+            //Action stack = () =>
+            //{
+            //    stacklist.Add(new SObject(new Wave()));
+            //    stacklist.Last().val.height = r.Next(1000000);
+            //};
+
+            //Stats.PrintOutTicks(wave, amount);
+            //Stats.PrintOutTicks(stack, amount);
+            //Stats.PrintOutTicks(wave, amount);
+            //Stats.PrintOutTicks(stack, amount);
+            //var dupa = 50;
             //new ExampleRun().Run();
             //new OperatorSQLExample().Run();
         }
